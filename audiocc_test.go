ee527f57ee393360ead7a815f76a5334a0eb05cf
Taylor <taylor@yahoo.com>
2018-06-28 10:41:28 -0500

    tests: remove unnecessary audiocc lastOfCurrentDir tests

--- audiocc_test.go
+++ audiocc_test.go
@@ -14,43 +14,6 @@ func TestProcessDirDNE(t *testing.T) {
   }
 }
 
-func TestLastOfCurrentDir(t *testing.T) {
-  paths := []string{
-    "/anything/nest/file3.ext",
-    "/anything/nest/file4.ext",
-    "/anything/file1.ext",
-    "/anything/file2.ext",
-  }
-
-  tests := []map[string]bool{
-    { "anything/nest": false },
-    { "anything/nest": true },
-    { "anything": false },
-    { "anything": true },
-  }
-
-  a := &audiocc{ DirEntry: "/anything", Files: paths }
-
-  for i := range tests {
-    for k, v := range tests[i] {
-      a.DirCur = k
-      r := a.lastOfCurrentDir(i)
-      if r != v {
-        t.Errorf("Expected %v, got %v", v, r)
-      }
-
-    }
-  }
-}
-
-func TestLastOfCurrentDirBounds(t *testing.T) {
-  a := &audiocc{ Files: []string{} }
-  r := a.lastOfCurrentDir(1)
-  if r != false {
-    t.Errorf("Expected %v, got %v", false, r)
-  }
-}
-
 func TestSkipArtistOnCollection(t *testing.T) {
   tests := []map[string][]bool{
     { "Jerry Garcia Band": { true, false } },
@@ -60,9 +23,9 @@ func TestSkipArtistOnCollection(t *testing.T) {
 
   for i := range tests {
     for k, v := range tests[i] {
-      flagCollection = v[0]
+      flags.Collection = v[0]
       defer func() {
-        flagCollection = false
+        flags.Collection = false
       }()
 
       r := skipArtistOnCollection(k)
