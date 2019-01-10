2b32edb43e5214aa87496cd288f6fc23e4de8c34
Taylor <taylor@yahoo.com>
2019-01-10 11:31:44 -0600

    audioc: processThreaded returns slice of metadata

--- audioc/thread.go
+++ audioc/thread.go
@@ -1,10 +1,14 @@
 package audioc
 
+import (
+  "github.com/jamlib/audioc/metadata"
+)
+
 // process each audio file within bundle in separate cpu thread
-func (a *audioc) processThreaded(indexes []int) (string, error) {
+func (a *audioc) processThreaded(indexes []int) ([]*metadata.Metadata, error) {
   var err error
   jobs := make(chan int)
-  dir := make(chan string, a.Workers)
+  data := make(chan []metadata.Metadata, a.Workers)
 
   // iterate through files sending them to worker processes
   go func() {
@@ -14,19 +18,20 @@ func (a *audioc) processThreaded(indexes []int) (string, error) {
       }
       jobs <- indexes[x]
     }
+
+    // close jobs channel once out of indexes
     close(jobs)
   }()
 
   // start worker processes
   for i := 0; i < a.Workers; i++ {
     go func() {
-      // TODO return slice of metadata instead of single dir string
-      // this allows full control over post processing
-      var d string
+      // build metadata slice
+      md := make([]metadata.Metadata, 0, len(indexes))
 
       for job := range jobs {
-        var e error
-        d, e = a.processFile(job)
+        m, e := a.processFile(job)
+        md = append(md, *m)
 
         // if single job errors, break & set shared err 
         if e != nil {
@@ -35,15 +40,19 @@ func (a *audioc) processThreaded(indexes []int) (string, error) {
         }
       }
 
-      dir <- d
+      data <- md
     }()
   }
 
   // wait for all workers to finish
-  var resultDir string
+  results := make([]*metadata.Metadata, 0, len(indexes))
   for i := 0; i < a.Workers; i++ {
-    resultDir = <-dir
+    workerMetadata := <-data
+    for x := range workerMetadata {
+      results = append(results, &workerMetadata[x])
+    }
   }
+  close(data)
 
-  return resultDir, err
+  return results, err
 }
